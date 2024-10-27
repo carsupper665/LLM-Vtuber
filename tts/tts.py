50f16448360be75d4915671d2c1701a4611fffd7
carsupper665 <c112156118@nkust.edu.tw>
2024-10-27 16:36:20 +0800

    rename

--- tts/tts.py
+++ tts/tts.py
@@ -7,7 +7,7 @@ class tts:
             from .EdgeTts import EdgeTTS
             return EdgeTTS(args.get('voice'), args.get('pitch'))
         elif tts_type == 'vitsTTS':
-            from .VistTts import VitsTTS
+            from .VitsTts import VitsTTS
             return VitsTTS(args.get('model_path'), args.get('config_path'), args.get('speed'))
         else:
             raise ValueError(f"[DeBug] [TTS] | Unknown TTS engine type: {tts_type}")
